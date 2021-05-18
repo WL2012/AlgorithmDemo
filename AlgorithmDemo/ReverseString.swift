77ee66f4016bd731dcd5129a40b6dfc87f3c5270
wangliang <283175161@qq.com>
2021-05-18 21:53:20 +0800

    初始化工程

---
+++ AlgorithmDemo/ReverseString.swift
@@ -0,0 +1,27 @@
+//
+//  ReverseString.swift
+//  AlgorithmDemo
+//
+//  Created by yangyangmao on 2021/5/18.
+//
+
+import Foundation
+
+func reverseString(str: String) -> String {
+    guard str.count > 1 else {
+        return str
+    }
+    ///"hello,world" 11
+    ///
+    var chars = str.utf8CString
+    print("\(str.count)===\(chars)")
+    var low = 0
+    var high = chars.count - 2
+    print(high)
+    while low < high {
+        chars.swapAt(low, high)
+        low += 1
+        high -= 1
+    }
+    return String(cString: Array(chars))
+}
--- AlgorithmDemo/ViewController.swift
+++ AlgorithmDemo/ViewController.swift
@@ -8,12 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let result = reverseString(str: "hello,world")
+        print(result)
     }
-
-
 }
 
